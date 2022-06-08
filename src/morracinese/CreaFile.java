d206d4610459faa719d7cf3f0d94c2ce958db8bd
Matteo <matteo.lub02@gmail.com>
2022-06-08 07:54:32 +0200

    Migliorato creaFile

--- src/morracinese/CreaFile.java
+++ src/morracinese/CreaFile.java
@@ -6,16 +6,19 @@ import java.io.PrintWriter;
 
 public class CreaFile {
 	public static void salvaSuFile (Partita partita) throws FileNotFoundException{
-		File nuovo = new File("statistiche.txt");
-		PrintWriter in = new PrintWriter(nuovo);
 		try {
+			File nuovo = new File("statistiche.txt");
+			PrintWriter in = new PrintWriter(nuovo);
 			in.println("Vittorie computer: " + partita.getNumeroVittorieComputer() + "\nVittorie giocatore: "
 					+ partita.getNumeroVittorieGiocatore() + "\nPareggi: " + partita.getNumeroPareggi());
+			in.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 		finally {
 			System.out.println("Vittorie computer: " + partita.getNumeroVittorieComputer() + "\nVittorie giocatore: "
 					+ partita.getNumeroVittorieGiocatore()+ "\nPareggi: " + partita.getNumeroPareggi());
-			in.close();
 		}
 	}
 }
